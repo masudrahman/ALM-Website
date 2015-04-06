6eb5d92c4ef89658e91e7ac89ee179f81b7cffeb
Aamir Rasheed <aamir1rasheed@gmail.com>
2015-04-05 22:53:53 -0700

    continued working on Bootstrap version - still unfinished

--- ALM Website Bootstrap/js/aamir.js
+++ ALM Website Bootstrap/js/aamir.js
@@ -1,55 +1,62 @@
-$(document).ready(function(){
+/* This entire section is for animating the page scroll button */
+	$(document).ready(function(){
 
-	$("#arrow-1").animate({
+		$("#arrow-1").animate({
 
-		marginTop: "-14px"
+			marginTop: "-14px"
 
-	}, 1000);
-	$("#arrow-2").animate({
+		}, 0);
+		$("#arrow-2").animate({
 
-		marginTop: "-14px"
+			marginTop: "-14px"
+			
+		}, 0);
+		setTimeout(
+		  function() 
+		  {
+		  	$("#arrow-3").animate({
+
+				top: "0px"
+				
+			}, 700);
+	 	}, 1000);
 		
-	}, 1000);
-	$("#arrow-3").animate({
 
-		marginTop: "-14px"
-		
-	}, 1000);
-});
+	});
 
-$("#page-scroll-button").hover(function(){
-	$("#arrow-1").animate({
+	$("#page-scroll-button").hover(function(){
+		$("#arrow-1").animate({
 
-		marginTop: "0px"
+			marginTop: "0px"
 
-	}, 200);
-	$("#arrow-2").animate({
+		}, 200);
+		$("#arrow-2").animate({
 
-		marginTop: "14px"
-		
-	}, 200);
-	$("#arrow-3").animate({
+			marginTop: "14px"
+			
+		}, 200);
+		$("#arrow-3").animate({
 
-		marginTop: "28px"
-		
-	}, 200);
-});
+			marginTop: "28px"
+			
+		}, 200);
+	});
 
-$("#page-scroll-button").mouseleave(function(){
-	$("#arrow-1").animate({
+	$("#page-scroll-button").mouseleave(function(){
+		$("#arrow-1").animate({
 
-		marginTop: "-14px"
+			marginTop: "-14px"
 
-	}, 200);
-	$("#arrow-2").animate({
+		}, 200);
+		$("#arrow-2").animate({
 
-		marginTop: "-14px"
-		
-	}, 200);
-	$("#arrow-3").animate({
+			marginTop: "-14px"
+			
+		}, 200);
+		$("#arrow-3").animate({
 
-		marginTop: "-14px"
-		
-	}, 200);
-});
-
+			marginTop: "0px"
+			
+		}, 200);
+	});
+/* end page scroll */
